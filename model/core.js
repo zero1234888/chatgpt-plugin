7a46912ebad6f4a21fc9944d84c7ee60d0f5acd1
Heavy <479067909@qq.com>
2025-01-04 16:10:21 +0800

    bugfix

--- model/core.js
+++ model/core.js
@@ -93,7 +93,6 @@ async function handleSystem (e, system) {
         ((opt.groupId) ? groupContextTip : '')
 
       system += `你的名字是“${opt.botName}”，你在一个qq群里，群号是${opt.groupId},当前和你说话的人群名片是${opt.nickname}, qq号是${opt.qq}, 请你结合用户的发言，本次聊天记录，以及历史聊天记录作出回应，要求表现得随性一点，最好参与讨论，混入其中。不要过分插科打诨，不要讨论过于久远的话题, 不知道说什么可以复读群友的话。要求你做搜索、发图、发视频和音乐等操作时要使用工具。不可以直接发[图片]这样蒙混过关。要求优先使用中文进行对话。` +
-          candidate +
           '以下是新增的聊天记录:' + chats
               .map(chat => {
                 let sender = chat.sender || chat || {}
@@ -804,7 +803,6 @@ class Core {
           ((Config.enableGroupContext && e.group_id) ? groupContextTip : '')
 
         system += `你的名字是“${botName}”，你在一个qq群里，群号是${e.group_id},当前和你说话的人群名片是${e.sender.card || e.sender.nickname}, qq号是${e.sender.user_id}, 请你结合用户的发言，本次聊天记录，以及历史聊天记录作出回应，要求表现得随性一点，最好参与讨论，混入其中。不要过分插科打诨，不要讨论过于久远的话题, 不知道说什么可以复读群友的话。要求你做搜索、发图、发视频和音乐等操作时要使用工具。不可以直接发[图片]这样蒙混过关。要求优先使用中文进行对话。` +
-            candidate +
             '以下是新增的聊天记录:' + chats
                 .map(chat => {
                   let sender = chat.sender || chat || {}
