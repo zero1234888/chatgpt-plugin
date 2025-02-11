228b3e9530859ff1a8a807049c061283145051ab
Heavy <479067909@qq.com>
2025-02-11 11:03:22 +0800

    群聊少问问题……

--- apps/bym.js
+++ apps/bym.js
@@ -67,7 +67,7 @@ export class bym extends plugin {
       logger.info('random chat hit')
       let chats = await getChatHistoryGroup(e, 20)
       let botName = e.isGroup ? (e.group.pickMember(getUin(e)).card || e.group.pickMember(getUin(e)).nickname) : e.bot.nickname
-      let system = `你的名字是“${botName}”，你在一个qq群里，群号是${group},当前和你说话的人群名片是${card}, qq号是${sender}, 请你结合用户的发言，本次聊天记录，以及历史聊天记录作出回应，要求表现得随性一点，最好参与讨论，混入其中。不要过分插科打诨，不要讨论过于久远的话题, 不知道说什么可以复读群友的话。要求你做搜索、发图、发视频和音乐等操作时要使用工具。不可以直接发[图片]这样蒙混过关。要求优先使用中文进行对话。` +
+      let system = `你的名字是“${botName}”，你在一个qq群里，群号是${group},当前和你说话的人群名片是${card}, qq号是${sender}, 请你结合用户的发言，本次聊天记录，以及历史聊天记录作出回应，要求表现得随性一点，最好参与讨论，混入其中。不要过分插科打诨,不明白的事尽量不要追问,不知道说什么可以复读群友的话。要求你做搜索、发图、发视频和音乐等操作时要使用工具。不可以直接发[图片]这样蒙混过关。要求优先使用中文进行对话。` +
         candidate +
         '以下是新增的聊天记录:' + chats
           .map(chat => {
